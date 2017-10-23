fcee5054baaaaf77e787cac5c7c3b0bfeb13a6c3
Michael Vilabrera <m.vilabrera@gmail.com>
2017-10-23 11:58:14 -0400

    start challenges

---
+++ Day10Chap12.playground/Contents.swift
@@ -0,0 +1,182 @@
+/*
+ Methods
+ */
+
+// method refresher
+
+var numbers = [1, 2, 3]
+numbers.removeLast()
+numbers
+
+// comparing methods to computed properties, turning a function into a method
+
+let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+//struct SimpleDate {
+//    var month: String
+//}
+//func monthsUntilWinterBreak(from date: SimpleDate) -> Int {
+//    return months.index(of: "December")! - months.index(of: date.month)!
+//}
+
+//struct SimpleDate {
+//    var month: String
+//    func monthsUntilWinterBreak(from date: SimpleDate) -> Int {
+//        return months.index(of: "December")! - months.index(of: date.month)!
+//    }
+//}
+//let thisMonth = SimpleDate(month: "October")
+//thisMonth.monthsUntilWinterBreak(from: thisMonth)
+//thisMonth.monthsUntilWinterBreak()  // not like this!
+
+// introducing self
+
+struct SimpleDate {
+    var month: String
+    func monthsUntilWinterBreak() -> Int {
+        return months.index(of: "December")! - months.index(of: self.month)!    // it is possible to remove 'self' here
+    }
+}
+let thisMonth = SimpleDate(month: "October")
+thisMonth.monthsUntilWinterBreak()  // its okay now
+
+// mini-exercise
+
+struct MiniExerciseSimpleDate {
+    var month: String
+    var monthsUntilWinterBreak: Int {
+        get {
+            return months.index(of: "December")! - months.index(of: month)!
+        }
+    }
+}
+let thatMonth = MiniExerciseSimpleDate(month: "July")
+thatMonth.monthsUntilWinterBreak
+
+// introducing initializers
+
+struct SimpleDateWithInit {
+    var month: String
+    init() {
+        month = "January"
+    }
+    func monthsUntilWinterBreak() -> Int {
+        return months.index(of: "December")! - months.index(of: month)!
+    }
+}
+let dateWithInit = SimpleDateWithInit()
+dateWithInit.month      // January
+dateWithInit.monthsUntilWinterBreak()
+
+// initializers in Structures
+
+struct SimpleDateWithInitInsideStruct {
+    var month: String
+    var day: Int
+    init() {
+        month = "January"
+        day = 1
+    }
+    init(month: String, day: Int) {
+        self.month = month
+        self.day = day
+    }
+    func monthsUntilWinterBreak() -> Int {
+        return months.index(of: "December")! - months.index(of: month)!
+    }
+}
+let valentinesDay = SimpleDateWithInitInsideStruct(month: "February", day: 14)
+
+// introducing mutating methods, type methods
+
+struct Math {
+    static func factorial(of number: Int) -> Int {
+        return (1...number).reduce(1, *)
+    }
+}
+Math.factorial(of: 7)
+
+// mini-exercise
+
+struct MathMiniExercise {
+    static func nthTriangleNumber(of number: Int) -> Int {
+        return (1...number).reduce(1, +)
+    }
+}
+MathMiniExercise.nthTriangleNumber(of: 7)
+
+// adding to an existing structure with extensions
+
+extension Math {
+    static func primeFactors(of value: Int) -> [Int] {
+        var remainingValue = value
+        var testFactor = 2
+        var primes: [Int] = []
+        while testFactor * testFactor <= remainingValue {
+            if remainingValue % testFactor == 0 {
+                primes.append(testFactor)
+                remainingValue /= testFactor
+            } else {
+                testFactor += 1
+            }
+        }
+        if remainingValue > 1 {
+            primes.append(remainingValue)
+        }
+        return primes
+    }
+}
+Math.primeFactors(of: 45)
+
+// keeping the compiler generated initializer using extensions
+
+struct LastSimpleDate {
+    var month: String
+    var day: Int
+    func monthsUntilWinterBreak() -> Int {
+        return months.index(of: "December")! - months.index(of: month)!
+    }
+    mutating func advance() {
+        day += 1
+    }
+}
+extension LastSimpleDate {
+    init() {
+        month = "January"
+        day = 1
+    }
+}
+
+/*
+ Challenges
+ */
+
+// 1
+struct Circle {
+    var radius = 0.0
+    var area: Double {
+        return .pi * radius * radius
+    }
+    init(radius: Double) {
+        self.radius = radius
+    }
+    mutating func grow(byFactor: Int) {
+        radius *= Double(byFactor).squareRoot()
+    }
+}
+var challengeCircle = Circle(radius: 5)
+challengeCircle.area
+challengeCircle.grow(byFactor: 4)
+challengeCircle.area
+
+// 2
+struct ChallengeSimpleDate {
+    var month: String
+    var day: Int
+    mutating func advance() {
+        if day != 30 && day != 31 {
+            day += 1
+        } else if day == 30 {
+            if month == ""
+        }
+    }
+}
