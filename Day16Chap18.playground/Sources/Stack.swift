eda56954ac077aebea83f343cf31aef12419e343
Michael Vilabrera <m.vilabrera@gmail.com>
2017-11-08 10:54:34 -0500

    reconfigured 'Stack' to remove error

--- Day16Chap18.playground/Sources/Stack.swift
+++ Day16Chap18.playground/Sources/Stack.swift
@@ -2,9 +2,10 @@ import Foundation
 
 class Stack<T> {
     private var content: [T]
-    private var top: T = content.last
+    private var top: T?
     init(content: [T]) {
         self.content = content
+        self.top = content.last
     }
     init(add: T) {
         self.content = [add]
