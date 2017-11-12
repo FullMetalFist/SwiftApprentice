e0e031decfa8cebe972733743af5995d19b7b94d
Michael Vilabrera <m.vilabrera@gmail.com>
2017-11-11 23:57:05 -0500

    keeping up streak 2

--- Day18Chap20.playground/Contents.swift
+++ Day18Chap20.playground/Contents.swift
@@ -1,3 +1,17 @@
 /*
  Pattern Matching
  */
+
+let coordinate = (x:1, y:0, z:0)
+
+// 1
+if (coordinate.y == 0) && (coordinate.z == 0) {
+    print("along the x axis")
+}
+
+// 2
+if case (_, 0, 0) = coordinate {
+    print("along the x axis")
+}
+
+// introducing patterns
