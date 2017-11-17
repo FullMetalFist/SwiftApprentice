6691ddafa8b7246797efc006dd144e9e7de127d7
Michael Vilabrera <m.vilabrera@gmail.com>
2017-11-17 14:01:44 -0500

    challenges complete except for first

--- Day18Chap20.playground/Contents.swift
+++ Day18Chap20.playground/Contents.swift
@@ -271,3 +271,278 @@ for level in levels {
     }
 }
 
+if case .animal(let legs) = pet, case 2...4 = legs {
+    print("potentially cuddly")
+} else {
+    print("not cuddly")
+}
+
+// chaining with commas
+
+enum Number {
+    case integerValue(Int)
+    case doubleValue(Double)
+    case booleanValue(Bool)
+}
+
+let a = 5
+let b = 6
+let c: Number? = .integerValue(7)
+let d: Number? = .integerValue(8)
+
+// pyramid of doom
+//if a != b {
+//    if let c = c {
+//        if let d = d {
+//            if case .integerValue(let cValue) = c {
+//                if case .integerValue(let dValue) = d {
+//                    if dValue > cValue {
+//                        print("a and b are different")
+//                        print("d is greater than c")
+//                        print("sum = \(a + b + cValue + dValue)")
+//                    }
+//                }
+//            }
+//        }
+//    }
+//}
+
+if a != b,
+    let c = c,
+    let d = d,
+    case .integerValue(let cValue) = c,
+    case .integerValue(let dValue) = d,
+    dValue > cValue {
+    print("a and b are different")
+    print("d is greater than c")
+    print("sum = \(a + b + cValue + dValue)")
+}
+
+// custom tuple
+
+let name = "Bob"
+let age = 23
+
+if case ("Bob", 23) = (name, age) {
+    print("Found our Bob")
+}
+
+var username: String?
+var password: String?
+
+switch (username, password) {
+case let (username?, password?):
+    print("Success! User: \(username) Pass: \(password)")
+case let (username?, nil):
+    print("Password is missing. User: \(username)")
+case let (nil, password?):
+    print("Username is missing. Pass: \(password)")
+case (nil, nil):
+    print("Both username and password are missing")
+}
+
+// fun with wildcards
+
+// do something multiple times
+
+for _ in 1...3 {
+    print("hi")
+}
+
+// validate than an optional exists
+
+let user: String? = "Bob"
+guard let _ = user else {
+    print("no user")
+    fatalError()
+}
+print("user exists, but identity not necessary")
+
+guard user != nil else {
+    print("no user")
+    fatalError()
+}
+
+// organize an if-else-if
+
+struct Rectangle {
+    let width: Int
+    let height: Int
+    let color: String
+}
+
+let view = Rectangle(width: 15, height: 60, color: "Blue")
+switch view {
+case _ where view.height < 50:
+    print("Shorter than 50 units")
+case _ where view.width > 20:
+    print("Over 50 units tall & over 20 units wide")
+case _ where view.color == "Blue":
+    print("Over 50 units tall, at most 20 units wide, and green")
+default:
+    print("this view can't be described")
+}
+
+// programming exercises
+
+// fibonacci
+
+func fibonacci(position: Int) -> Int {
+    switch position {
+    case let n where n <= 1:
+        return 0
+    case 2:
+        return 1
+    case let n:
+        return fibonacci(position: n - 1) + fibonacci(position: n - 2)
+    }
+}
+
+let fib15 = fibonacci(position: 15)
+
+// fizz buzz
+
+for i in 1...100 {
+    switch (i % 3, i % 5) {
+    case (0, 0):
+        print("FizzBuzz", terminator: " ")
+    case (0, _):
+        print("Fizz", terminator: " ")
+    case (_, 0):
+        print("Buzz", terminator: " ")
+    case (_, _):
+        print(i, terminator: " ")
+    }
+}
+print("")
+
+// expression pattern
+
+let matched = (1...10 ~= 7)
+
+if case 1...10 = 7 {
+    print("in range")
+}
+
+// overloading ~=
+
+let list = [0, 1, 2, 3]
+let integer = 2
+
+//let isInArray = (list ~= integer)
+
+//if case list = integer {
+//    print("our integer is here")
+//} else {
+//    print("our integer is not here")
+//}
+
+let isInList = list.contains(integer)
+
+func ~=(pattern: [Int], value: Int) -> Bool {
+    for i in pattern {
+        if i == value {
+            return true
+        }
+    }
+    return false
+}
+
+let isInArray = (list ~= integer)
+
+if case list = integer {
+    print("our integer is here")
+} else {
+    print("our integer is not here")
+}
+
+/*
+ Challenges
+ */
+
+// 1
+
+enum FormField {
+    case firstName(String)
+    case lastName(String)
+    case emailAddress(String)
+    case age(Int)
+}
+let minimumAge = 21
+let submittedAge = FormField.age(22)
+
+//if case .age(let x), where .age(x > 21): {
+//
+//}
+
+// 2
+
+enum CelestialBody {
+    case star
+    case planet(liquid: Bool)
+    case comet
+}
+
+let telescopeCensus = [
+    CelestialBody.star,
+    CelestialBody.planet(liquid: false),
+    CelestialBody.planet(liquid: true),
+    CelestialBody.planet(liquid: true),
+    CelestialBody.comet
+]
+
+for body in telescopeCensus {
+    switch body {
+    case .planet(let x):
+        if x {
+            print("we found water")
+        } else {
+            print("no water")
+        }
+    default:
+        print("not interested")
+    }
+}
+
+// 3
+
+let queenAlbums = [
+    ("A night at the opera", 1974),
+    ("Sheer Heart Attack", 1974),
+    ("Jazz", 1978),
+    ("The Game", 1980)
+]
+
+var albumFrom1974 = 0
+
+for album in queenAlbums {
+    switch album {
+    case (_, 1974):
+        albumFrom1974 += 1
+    default:
+        break
+    }
+}
+print(albumFrom1974)
+
+// 4
+
+let coordinates = (lat: 192.89483, long: -68.887463)
+
+// northern hemisphere (+ lat)
+// southern hemisphere (- lat)
+// on equator (0 lat)
+switch coordinates {
+case (let x, _):
+    switch x {
+    case _ where x < 0:
+        print("southern hemisphere")
+    case _ where x > 0:
+        print("northern hemisphere")
+    default:
+        print("on equator")
+        break
+    }
+default:
+    break
+}
